44d39263ee6e65295c9239610cf6fddbb7b39202
Elena Kulakova <kulakovaelena15@gmail.com>
2019-11-02 14:42:35 +0100

    Initial Commit

---
+++ Dice Programatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice Programatically
+//
+//  Created by Darko Kulakov on 2019-11-02.
+//  Copyright © 2019 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
